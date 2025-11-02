45b82f7876cacbd6ce9b4b971c3a0eb3f5ed1404
chiquitblanch <gangulhange@gmail.com>
2025-11-02 14:23:15 +0800

    initial project setup

---
+++ frontend/abi/ClaimEvaluatorAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+  Generated from: D:\self\zama\over\ClaimEvaluator-1\backend\deployments
+*/
+export const ClaimEvaluatorAddresses = { 
+  "11155111": { address: "0x8750236AA4B2737b89E8E5b6AE881852376181D7", chainId: 11155111, chainName: "sepolia" }
+};
